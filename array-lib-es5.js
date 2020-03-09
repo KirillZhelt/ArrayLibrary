cbe2e08c924aee79520ba7d208ec6b1ca3b7a124
Kirill <kirillzhelt@gmail.com>
2020-03-09 10:06:46 +0300

    Implement basic functionality using ES5

--- array-lib-es5.js
+++ array-lib-es5.js
@@ -1,3 +1,43 @@
-"use strict"
+'use strict'
 
-var arrayLib = {};
+var arrayLibES5 = {};
+
+arrayLibES5.take = function(array, n) {
+  return array.slice(0, n);
+};
+
+arrayLibES5.skip = function(array, n) {
+  return array.slice(n, array.length);
+};
+
+arrayLibES5.map = function(array, callback) {
+  var resultArray = []; 
+  this.foreach(array, function(item) {
+    resultArray.push(callback(item));
+  });
+  return resultArray;
+};
+
+arrayLibES5.reduce = function(array, callback, initialValue) {
+  var resultValue = initialValue;
+  this.foreach(array, function(item) {
+    resultValue = callback(resultValue, item);
+  });
+  return resultValue;
+};
+
+arrayLibES5.filter = function(array, callback) {
+  var resultArray = [];
+  this.foreach(array, function(item) {
+    if (callback(item)) {
+      resultArray.push(item);
+    }
+  });
+  return resultArray;
+};
+
+arrayLibES5.foreach = function(array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+};
